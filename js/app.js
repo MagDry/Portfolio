8311d601c1e6078089aae2ea6e40ed6aa5b14b4b
MagDry <magdalena.drymajlo@gmail.com>
2018-11-05 21:59:01 +0100

    v1

---
+++ js/app.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+//buttons
+
+let leftBtn = document.querySelector('.sliderButtonPrev');
+//console.log(leftBtn);
+let rightBtn = document.querySelector('.sliderButtonNext');
+//console.log(rightBtn);
+
+//images list - first image
+let imgClass = document.querySelectorAll('.image');
+//console.log(imgClass);
+
+//define a variable
+let index = 0;
+
+
+//add "visible" class to image
+imgClass[index].classList.add('visible');
+imgClass[index+1].classList.add('visible');
+imgClass[index+2].classList.add('visible');
+
+
+
+//press right button - "visible" class is removed
+rightBtn.addEventListener('click', function(){
+
+        imgClass[index].classList.remove('visible');        
+        imgClass[index+1].classList.remove('visible');        
+        imgClass[index+2].classList.remove('visible');        
+            index++;
+        if (index==6) {
+            index=0;
+        }
+        imgClass[index].classList.add('visible');
+        imgClass[index+1].classList.add('visible');        
+        imgClass[index+2].classList.add('visible');
+    
+});
+
+
+leftBtn.addEventListener('click', function(){
+
+    imgClass[index].classList.remove('visible');
+    imgClass[index+1].classList.remove('visible');
+    imgClass[index+2].classList.remove('visible');
+        index--;
+    if(index==-1){
+        index=imgClass.length-3;
+    }
+    imgClass[index].classList.add('visible');
+    imgClass[index+1].classList.add('visible');
+    imgClass[index+2].classList.add('visible');
+});
+
+
+});
+
